45eb2c436bb7c8b4c64038c3adc2f61671452447
DESKTOP-C66Q3BT\ruifs <rui@dulongo.com>
2020-12-22 01:58:30 +0000

    Added methods into the domain classes

---
+++ TaskerAI.Domain/Availability.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("TaskerAI.Application")]
+namespace TaskerAI.Domain
+{
+
+    public class Availability
+    {
+
+        public int Id { get; private set; }
+        public DateTimeOffset StartDate { get; private set; }
+        public DateTimeOffset EndDate { get; private set; }
+
+        public Assignee User { get; private set; }
+
+    public Availability(int id, DateTimeOffset startDate, DateTimeOffset endDate, Assignee user)
+        {
+            this.Id = id;
+            this.StartDate = startDate;
+            this.EndDate = endDate;
+            this.User = user;
+        }
+
+
+
+    }
+
+
+}
