449b374131417a36f3ab6105e5ed6c96dd775e65
DESKTOP-C66Q3BT\ruifs <rui@dulongo.com>
2021-01-06 10:48:20 +0000

    new plan creation function (WIP)

--- TaskerAI.Application/PlanBuilder.cs
+++ TaskerAI.Application/PlanBuilder.cs
@@ -81,73 +81,17 @@
         }
 
 
-        public Plan CreatePlan2(PlanBuilderContext context)
+        public List<Plan> GeneratePlans(PlanBuilderContext context)
         {
             List<TaskRoute> taskRoutes = this.taskRouteRepository.GetRoutes(context.Tasks, context.Tasks);
 
-            int planTotalTime = 0;
-            var resultRoutes = new List<TaskRoute>();
-            var inputTaskList = new List<Task>(context.Tasks);
+            List<Plan> resultList = new List<Plan>();
 
-            Task firstTask = inputTaskList.Where(t => t.Date == DateTimeOffset.Now.Date)
-                                         .OrderBy(t => t.DueDate)
-                                         .FirstOrDefault();
+          
 
-            if (firstTask != null)
-            {
-                return null;
-            }
+            resultList = CreatePlans(Plan.Create(taskRoutes, context.PlanStartDate) , taskRoutes, resultList, context);
 
-            Task currentTask = firstTask;
-
-            while (inputTaskList.Count > 0 && resultRoutes.Count < context.MaxTaskNumber && (context.MaxTimeInSeconds == null || context.MaxTimeInSeconds > planTotalTime))
-            {
-                var nextTaskRoutes =
-                (
-                    from tr in taskRoutes
-                    where tr.From.Id == currentTask.Id
-                    orderby tr.TimeInSeconds
-                    select tr
-                 ).ToList();
-
-                if (resultRoutes.Count == 0)
-                {
-                    foreach (TaskRoute route in nextTaskRoutes)
-                    {
-                        route.Estimate(context.PlanStartDate);
-                    }
-                }
-
-                TaskRoute taskRoute =
-                (
-                    from tr in nextTaskRoutes
-                    let nextTaskEstimatedDueDate = tr.EstimatedEndDate.AddSeconds(tr.TimeInSeconds).AddSeconds(tr.From.DurationInSeconds)
-                    where tr.From.DueDate >= nextTaskEstimatedDueDate
-                    orderby tr.TimeInSeconds
-                    select tr
-                ).FirstOrDefault();
-
-                if (taskRoute == null)
-                {
-                    taskRoute = nextTaskRoutes.OrderBy(t => t.TimeInSeconds).FirstOrDefault();
-                }
-
-                if (resultRoutes.Count != 0)
-                {
-                    taskRoute.Estimate(taskRoute.EstimatedEndDate.AddSeconds(taskRoute.TimeInSeconds));
-                }
-
-                inputTaskList.Remove(taskRoute.From);
-                taskRoutes.RemoveAll(t => t.From.Id == firstTask.Id || t.To.Id == firstTask.Id);
-
-                planTotalTime += taskRoute.RouteExecutionTimeInSeconds;
-
-                currentTask = taskRoute.To;
-
-                resultRoutes.Add(taskRoute);
-            }
-
-            return Plan.Create(resultRoutes, context.PlanStartDate);
+            return resultList;
         }
 
 
